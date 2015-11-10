f84d93ea90cb42551ec7b801423b9caf8960c04b
Cameron McCormack <cam@mcc.id.au>
2015-11-10 17:10:33 +1100

    Add enum to represent whether a style system implementation is backed by Gecko or Servo objects.

---
+++ layout/style/StyleImplementation.h
@@ -0,0 +1,14 @@
+#ifndef nsStyleImplementation_h
+#define nsStyleImplementation_h
+
+namespace mozilla {
+
+enum class StyleImplementation : int
+{
+  Gecko,
+  Servo
+};
+
+} // namespace mozilla
+
+#endif // nsStyleImplementation_h
