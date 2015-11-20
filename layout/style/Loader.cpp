b6c37e0b519df3e48ac7c80777edf37383c701d3
Cameron McCormack <cam@mcc.id.au>
2015-11-20 12:14:56 +1100

    Add placeholders/assertions for Servo-backed style sheet and style="" attribute parsing.

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -1737,19 +1737,23 @@ Loader::ParseSheet(const nsAString& aInput,
 
   aCompleted = false;
 
-  // XXXheycam ServoStyleSheets don't support parsing their contents yet.
-  MOZ_ASSERT(aLoadData->mSheet->IsGecko(),
-             "stylo: can't parse ServoStyleSheets contents yet");
-  nsCSSParser parser(this, aLoadData->mSheet->AsGecko());
-
   // Push our load data on the stack so any kids can pick it up
   mParsingDatas.AppendElement(aLoadData);
   nsIURI* sheetURI = aLoadData->mSheet->GetSheetURI();
   nsIURI* baseURI = aLoadData->mSheet->GetBaseURI();
-  nsresult rv = parser.ParseSheet(aInput, sheetURI, baseURI,
-                                  aLoadData->mSheet->Principal(),
-                                  aLoadData->mLineNumber,
-                                  aLoadData->mParsingMode);
+
+  nsresult rv;
+
+  if (aLoadData->mSheet->IsGecko()) {
+    nsCSSParser parser(this, aLoadData->mSheet->AsGecko());
+    rv = parser.ParseSheet(aInput, sheetURI, baseURI,
+                           aLoadData->mSheet->Principal(),
+                           aLoadData->mLineNumber,
+                           aLoadData->mParsingMode);
+  } else {
+    MOZ_CRASH("stylo: can't parse ServoStyleSheet contents yet");
+  }
+
   mParsingDatas.RemoveElementAt(mParsingDatas.Length() - 1);
 
   if (NS_FAILED(rv)) {
