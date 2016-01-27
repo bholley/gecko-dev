71e61d776bd5764178a98df6ea2b3fbc1d5bb3b9
Bobby Holley <bobbyholley@gmail.com>
2016-01-26 17:25:16 -0800

    Hack: Delay layout initialization until parsing finishes and force a restyle when it happens.

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -62,6 +62,12 @@ ServoStyleSet::EndUpdate()
   return NS_OK;
 }
 
+void
+ServoStyleSet::ForceRestyle(nsPresContext* aPresContext)
+{
+  Servo_RestyleDocument(aPresContext->Document());
+}
+
 // resolve a style context
 already_AddRefed<nsStyleContext>
 ServoStyleSet::ResolveStyleFor(Element* aElement,
