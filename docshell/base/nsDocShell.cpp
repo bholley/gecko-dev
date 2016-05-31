e72f7c9f6a8890c66445d878f742b5b9c1183337
Cameron McCormack <cam@mcc.id.au>
2016-05-31 16:55:02 +1000

    Hack: Add UI for stylo experimentation.

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -813,6 +813,7 @@ nsDocShell::nsDocShell()
   , mFrameType(FRAME_TYPE_REGULAR)
   , mPrivateBrowsingId(0)
   , mParentCharsetSource(0)
+  , mWantsStylo(true)
   , mJSRunToCompletionDepth(0)
 {
   AssertOriginAttributesMatchPrivateBrowsing();
